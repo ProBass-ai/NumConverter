5b4d32c58e3609d1c0729a96a4fe484402ad542e
tlefatol021/prothedev <tlefatol021@student.wethinkcode.co.za>
2022-09-07 09:41:58 +0200

    /

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args){
-        Scanner scanner;
-        String converts;
+    static Scanner scanner;
+    static String converts;
 
 
+    public static boolean checkFloat(String n){
+        try{
+            return (Float.parseFloat(n) >= 0) && (Float.parseFloat(n) < 100000);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
+
+    public static boolean checkInt(String n){
+        try{
+            return (Integer.parseInt(n)) >= 0 && (Integer.parseInt(n) < 100000);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static void runConverter(String userIn){
         scanner = new Scanner(System.in);
-        System.out.println("Welcome to the decimal-point converter...");
-
-        System.out.println("Which convertion would you like to perform?");
-        System.out.println("1. Decimal to HexaDecimal?");
-        System.out.println("2. 10th-Decimal point to 2nd-Decimal point?");
-        System.out.println("3. 2nd-Decimal point to 10th-Decimal point?");
-        String userIn = scanner.nextLine();
-        System.out.println("\n");
-
 
         switch (userIn) {
 
@@ -43,7 +49,7 @@ public class Main {
                 System.out.print("Enter number to convert: ");
                 userIn = scanner.next();
 
-                while (!checkDouble(userIn)){
+                while (!checkFloat(userIn)){
                     System.out.println("Please Enter a number between 0 and 10000");
                     userIn = scanner.nextLine();
                 }
@@ -61,7 +67,7 @@ public class Main {
                 System.out.print("Enter number to convert: ");
                 userIn = scanner.next();
 
-                while (!checkDouble(userIn)){
+                while (!checkFloat(userIn)){
                     System.out.println("Please Enter a number between 0 and 10000");
                     userIn = scanner.nextLine();
                 }
@@ -74,26 +80,32 @@ public class Main {
                 System.out.println(converts);
             }
         }
+    }
+
+
+    public static void main(String[] args){
+
+        scanner = new Scanner(System.in);
+        System.out.println("Welcome to the decimal-point converter...");
+
+        System.out.println("""
+                Which convertion would you like to perform?
+                1. Decimal to HexaDecimal?
+                2. 10th-Decimal point to 2nd-Decimal point?
+                3. 2nd-Decimal point to 10th-Decimal point?""");
+
+        String userIn = scanner.nextLine();
+        System.out.println("-------------------------------------------------------------");
+
+        runConverter(userIn);
+
+
 
 
 
     }
 
-    public static boolean checkDouble(String n){
-        try{
-            return (Double.parseDouble(n) >= 0) && (Double.parseDouble(n) < 100000);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
 
-    public static boolean checkInt(String n){
-        try{
-            return (Integer.parseInt(n)) >= 0 && (Integer.parseInt(n) < 100000);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
 
 
 }
